ebed96044d9d3d823f6e3429f977aae1175302a7
aliensanderdiaz <aliensanderdiaz@gmail.com>
2023-06-27 17:52:48 -0500

    fin curso

--- db/models/product.model.js
+++ db/models/product.model.js
@@ -1,9 +1,10 @@
 const { Model, DataTypes, Sequelize } = require('sequelize')
-const { USER_TABLE } = require('./user.model')
 
-const CUSTOMER_TABLE = 'customers'
+const { CATEGORY_TABLE } = require('./category.model')
 
-const CustomerSchema = {
+const PRODUCT_TABLE = 'products'
+
+const ProductSchema = {
   id: {
     allowNull: false,
     autoIncrement: true,
@@ -12,16 +13,20 @@ const CustomerSchema = {
   },
   name: {
     allowNull: false,
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    unique: true
   },
-  lastName: {
+  image: {
     allowNull: false,
     type: DataTypes.STRING,
-    field: 'last_name'
   },
-  phone: {
+  description: {
     allowNull: false,
-    type: DataTypes.STRING
+    type: DataTypes.TEXT,
+  },
+  price: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
   },
   createdAt: {
     allowNull: false,
@@ -29,13 +34,12 @@ const CustomerSchema = {
     field: 'created_at',
     defaultValue: Sequelize.NOW
   },
-  userId: {
-    field: 'user_id',
+  categoryId: {
+    field: 'category_id',
     allowNull: false,
     type: DataTypes.INTEGER,
-    unique: true,
     references: {
-      model: USER_TABLE,
+      model: CATEGORY_TABLE,
       key: 'id'
     },
     onUpdate: 'CASCADE',
@@ -43,23 +47,19 @@ const CustomerSchema = {
   }
 }
 
-class Customer extends Model {
+class Product extends Model {
   static associate(models) {
-    this.belongsTo(models.User, {as: 'user'})
-    this.hasMany(models.Order, {
-      as: 'orders',
-      foreignKey: 'customerId'
-    })
+    this.belongsTo(models.Category, {as: 'category'})
   }
 
   static config(sequelize) {
     return {
       sequelize,
-      tableName: CUSTOMER_TABLE,
-      modelName: 'Customer',
+      tableName: PRODUCT_TABLE,
+      modelName: 'Product',
       timestamps: false
     }
   }
 }
 
-module.exports = { CUSTOMER_TABLE, CustomerSchema, Customer }
+module.exports = { PRODUCT_TABLE, ProductSchema, Product }
--- services/order.service.js
+++ services/order.service.js
@@ -1,19 +1,41 @@
 const boom = require('@hapi/boom');
 
+const { models } = require('./../libs/sequelize')
+
 class OrderService {
 
   constructor(){
   }
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data)
+    return newOrder;
+  }
+
+  async addItem(data) {
+    const newItem = await models.OrderProduct.create(data)
+    return newItem;
   }
 
   async find() {
-    return [];
+    const rta = await models.Order.findAll({
+      include: ['customer']
+    })
+    return rta;
   }
 
   async findOne(id) {
-    return { id };
+    const order = await models.Order.findByPk(id, {
+      include: [{
+        association: 'customer',
+        include: [ 'user']
+      },
+      'items'
+    ]
+    })
+    if (!order) {
+      throw boom.notFound('order not found')
+    }
+    return order;
   }
 
   async update(id, changes) {
